9240df0b7e55c7f3522d85f809cce37982d51f71
Zaid Javed <zaidjaved786@gmail.com>
2020-09-07 18:29:46 +0500

    Organic Farm Project

---
+++ assets/works/Organic Farm/js/map-script.js
@@ -0,0 +1,138 @@
+"use strict";
+
+
+function googleMap () {
+  if ($('.google-map-three').length) {
+    $('.google-map-three').each(function () {
+      // getting options from html 
+      var Self = $(this);
+      var mapName = Self.attr('id');
+      var mapLat = Self.data('map-lat');
+      var mapLng = Self.data('map-lng');
+      var iconPath = Self.data('icon-path');
+      var mapZoom = Self.data('map-zoom');
+      var mapTitle = Self.data('map-title');
+
+
+	  var styles = [
+	    {
+	        "featureType": "landscape",
+	        "elementType": "labels",
+	        "stylers": [
+	            {
+	                "visibility": "off"
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "transit",
+	        "elementType": "labels",
+	        "stylers": [
+	            {
+	                "visibility": "off"
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "poi",
+	        "elementType": "labels",
+	        "stylers": [
+	            {
+	                "visibility": "off"
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "water",
+	        "elementType": "labels",
+	        "stylers": [
+	            {
+	                "visibility": "off"
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "road",
+	        "elementType": "labels.icon",
+	        "stylers": [
+	            {
+	                "visibility": "off"
+	            }
+	        ]
+	    },
+	    {
+	        "stylers": [
+	            {
+	                "hue": "#00aaff"
+	            },
+	            {
+	                "saturation": -100
+	            },
+	            {
+	                "gamma": 2.15
+	            },
+	            {
+	                "lightness": 12
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "road",
+	        "elementType": "labels.text.fill",
+	        "stylers": [
+	            {
+	                "visibility": "on"
+	            },
+	            {
+	                "lightness": 24
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "road",
+	        "elementType": "geometry",
+	        "stylers": [
+	            {
+	                "lightness": 57
+	            }
+	        ]
+	    }
+	]
+
+
+      // if zoom not defined the zoom value will be 15;
+      if (!mapZoom) {
+        var mapZoom = 12;
+      };
+      // init map
+      var map;
+      map = new GMaps({
+          div: '#'+mapName,
+          scrollwheel: false,
+          lat: mapLat,
+          lng: mapLng,
+          styles: styles,
+          zoom: mapZoom
+      });
+      // if icon path setted then show marker
+      if(iconPath) {
+        
+        map.addMarker({
+            icon: 'images/logo/map.png',
+            lat: 40.925372,
+              lng: -74.276544,
+              title: 'Ollo Main Office',
+        });
+      }
+    });  
+  };
+}
+
+
+// Dom Ready Function
+jQuery(document).on('ready', function () {
+	(function ($) {
+		// add your functions
+		googleMap();
+	})(jQuery);
+});
